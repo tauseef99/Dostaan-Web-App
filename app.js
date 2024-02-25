5733cbdb4c4b7e004070819c6fc0b701dbe1b3ec
tauseef hussain <tauseefhussain232@gmail.com>
2024-02-25 16:34:59 +0500

    second commit 3

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.port || 4003;
 const mongoose = require("mongoose");
 const { mongoUrl } = require("./keys");
 const cors = require("cors");
