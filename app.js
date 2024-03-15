09e038909e8999d1e9819f2068f000bfe01cebcc
tauseef hussain <tauseefhussain232@gmail.com>
2024-03-15 11:17:29 +0500

    updated mogo url as enviroment updation

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.port || 5000;
 const mongoose = require("mongoose");
-const { mongoUrl } = require("./keys");
+// const { mongoUrl } = require("./keys");
 const cors = require("cors");
 const path = require("path");
 
--- keys.js
+++ keys.js
@@ -1,8 +1,4 @@
-
-
-
-
-module.exports = {
-    mongoUrl : "mongodb+srv://tauseefhussain232:qwer1234.@cluster0.gvmeu8z.mongodb.net/",
-    jwt_secret : "asdadsadasdsa"
-};
+// module.exports = {
+//     mongoUrl : "mongodb+srv://tauseefhussain232:qwer1234.@cluster0.gvmeu8z.mongodb.net/",
+//     jwt_secret : "asdadsadasdsa"
+// };
