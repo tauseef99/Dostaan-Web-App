e83d95444c390ce000bdca28a1055e0c8369cd1c
tauseef hussain <tauseefhussain232@gmail.com>
2024-03-15 09:46:03 +0500

    serving frontend in backend

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.port || 5000;
 const mongoose = require("mongoose");
 const { mongoUrl } = require("./keys");
 const cors = require("cors");
@@ -16,18 +16,17 @@ app.use(require("./routes/user"));
 mongoose.connect(mongoUrl);
 
 mongoose.connection.on("connected", () => {
-  console.log("Successfully connected to MongoDB");
-});
-mongoose.connection.on("error", (err) => {
-  console.error("Connection to MongoDB failed:", err);
+  console.log("successfully connected to mongo");
 });
 
-// Serve static files from the React app
+mongoose.connection.on("error", () => {
+  console.log("not connected to mongodb");
+});
+
+// serving the frontend into backend
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 
-// Route all requests to React app's index.html
-app.use(express.static(path.join(__dirname, "./frontend/build")));
-app.get("*", function (_, res) {
+app.get("*", (req, res) => {
   res.sendFile(
     path.join(__dirname, "./frontend/build/index.html"),
     function (err) {
@@ -35,16 +34,7 @@ app.get("*", function (_, res) {
     }
   );
 });
-// app.get("*", (req, res) => {
-//   try {
-//     const indexPath = path.join(__dirname, "./frontend/build/index.html");
-//     res.sendFile(indexPath);
-//   } catch (err) {
-//     console.error("Error sending index.html:", err);
-//     res.status(500).send("Internal Server Error");
-//   }
-// });
 
 app.listen(port, () => {
-  console.log("Server is running on port " + port);
+  console.log("server is running on port" + " " + port);
 });
